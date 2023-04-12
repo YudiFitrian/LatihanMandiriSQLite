60939038c0fe5d5fedb5da366dd2989fb813b790
Yudi Fitriansyah <cleveryf098@gmail.com>
2023-04-12 16:46:34 +0700

    Latihan(1)

---
+++ app/src/main/java/yudifitrian/si61/si/latihanmandirisqlite/MainActivity.java
@@ -0,0 +1,14 @@
+package yudifitrian.si61.si.latihanmandirisqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
